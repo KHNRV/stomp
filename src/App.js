7cdd3d433e9f259139609509f3cbc530c5781cb1
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-12 10:34:44 -0400

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Nav from "./components/Nav";
 import Competition from "./components/Competition";
 import ScoringTable from "./components/ScoringTable";
 import ResultsTable from "./components/ResultsTable";
@@ -14,25 +15,31 @@ function App() {
   const [data, setData] = useState(compData);
   const [columns, setColumns] = useState(compCol);
 
-
   return (
     <div className="App">
-      <h1>Stomp</h1>
-      <Router>
-        <Stepper />
-        <Switch>
-          <Route exact path="/competitions/:id/">
-            <Competition data={data} setData={setData} columns={columns} setColumns={setColumns} />
-          </Route>
-          <Route exact path="/competitions/:id/scoring">
-            <ScoringTable data={data} setData={setData} columns={columns} />
-          </Route>
-          <Route exact path="/competitions/:id/results">
-            <ResultsTable data={data} columns={columns} />
-          </Route>
-          <Route path="/"></Route>
-        </Switch>
-      </Router>
+      <Nav></Nav>
+      <div className="main-components">
+        <Router>
+          <Stepper />
+          <Switch>
+            <Route exact path="/competitions/:id/">
+              <Competition
+                data={data}
+                setData={setData}
+                columns={columns}
+                setColumns={setColumns}
+              />
+            </Route>
+            <Route exact path="/competitions/:id/scoring">
+              <ScoringTable data={data} setData={setData} columns={columns} />
+            </Route>
+            <Route exact path="/competitions/:id/results">
+              <ResultsTable data={data} columns={columns} />
+            </Route>
+            <Route path="/"></Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Icon from "@material-ui/core/Icon";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(4),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Nav() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" className={classes.title}>
+            Stomp
+          </Typography>
+          {true ? ( //FOR now set true false for logged in
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              endIcon={<Icon>dashboard</Icon>}
+            >
+              Dashboard
+            </Button>
+          ) : (
+            <div className="user-notlogged">
+              <Button color="inherit">Login</Button>
+              <Button color="inherit">Sign up</Button>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
