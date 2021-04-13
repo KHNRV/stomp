2bc485a35c91952355b6d62c0ba8a1b932edb6e2
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-13 19:28:28 -0400

    send Data functionality

--- src/helpers/sendData.js
+++ src/helpers/sendData.js
@@ -1,10 +1,45 @@
-import {expCompCol, expCompData} from './testdata'
+const sendData = (data, columns) => {
+  const result = {
+    id: 1,
+    name: "Open Solo Jazz - Prelims",
+    scoring_system_id: 1,
+    competitors: [
+      /*Populated*/
+    ],
+    judges: [
+      /*Populated*/
+    ],
+    scores: [
+      /*Populated*/
+    ],
+  };
 
-// const sendData = () => {
+  let judgeArr = columns.slice(4);
+  judgeArr.forEach((j) => {
+    result.judges.push(parseInt(j.field));
+  });
 
-// }
+  data.forEach((e) => {
+    result.competitors.push(parseInt(e.id));
+    const participantId = e.id;
 
-expCompCol
+    delete e["id"];
+    delete e["bib"];
+    delete e["first_name"];
+    delete e["last_name"];
 
+    const jIds = Object.keys(e);
+    const jScores = Object.values(e);
 
-expCompData
+    for (let i = 0; i < jIds.length - 1; i++) {
+      result.scores.push({
+        participant_id: participantId,
+        judge_id: parseInt(jIds[i]),
+        value: parseInt(jScores[i]),
+      });
+    }
+  });
+  return result;
+};
+
+export default sendData;
