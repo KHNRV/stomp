739c1b904607973a38a250875f0b61eb85b366f7
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-13 21:15:09 -0400

    change class name

--- src/components/DashboardCompetitions.js
+++ src/components/DashboardCompetitions.js
@@ -4,6 +4,9 @@ import DashboardCompetitionsNew from "./DashboardCompetitionsNew";
 import { NavLink } from "react-router-dom";
 import MaterialTable from "@material-table/core";
 
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "../theme";
+
 const DashboardCompetitions = () => {
   // DBLOGIC
   const columns = [
@@ -36,54 +39,56 @@ const DashboardCompetitions = () => {
   ]);
 
   return (
-    <div className="competitions">
-      <MaterialTable
-        columns={columns}
-        data={comps}
-        style={{ padding: "0.5em", backgroundColor: "#F8F8F8" }}
-        localization={{
-          body: {
-            emptyDataSourceMessage: "There are no Competitions",
-          },
-        }}
-        options={{
-          search: false,
-          headerStyle: {
-            backgroundColor: "#dfad3e",
-            color: "#08253f",
-          },
-          toolbarButtonAlignment: "right", // here is the option to change toolbar buttons' alignment
-          padding: "default",
-          paging: false,
-          actionsColumnIndex: -1,
-          fixedColumns: { left: 0, right: 0 },
-        }}
-        components={{
-          Toolbar: (props) => (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row-reverse",
-                paddingBottom: "10px",
-              }}
-            >
-              <DashboardCompetitionsNew />
-            </div>
-          ),
-        }}
-        editable={{
-          onRowDelete: (oldData) =>
-            new Promise((resolve, reject) => {
-              setTimeout(() => {
-                const dataDelete = [...comps];
-                const index = oldData.tableData.id;
-                dataDelete.splice(index, 1);
-                setComps([...dataDelete]);
-                resolve();
-              }, 1000);
-            }),
-        }}
-      />
+    <div className="results-table">
+      <ThemeProvider theme={theme}>
+        <MaterialTable
+          columns={columns}
+          data={comps}
+          style={{ padding: "0.5em", backgroundColor: "#F8F8F8" }}
+          localization={{
+            body: {
+              emptyDataSourceMessage: "There are no Competitions",
+            },
+          }}
+          options={{
+            search: false,
+            headerStyle: {
+              backgroundColor: "#dfad3e",
+              color: "#08253f",
+            },
+            toolbarButtonAlignment: "right", // here is the option to change toolbar buttons' alignment
+            padding: "default",
+            paging: false,
+            actionsColumnIndex: -1,
+            fixedColumns: { left: 0, right: 0 },
+          }}
+          components={{
+            Toolbar: (props) => (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                  paddingBottom: "10px",
+                }}
+              >
+                <DashboardCompetitionsNew />
+              </div>
+            ),
+          }}
+          editable={{
+            onRowDelete: (oldData) =>
+              new Promise((resolve, reject) => {
+                setTimeout(() => {
+                  const dataDelete = [...comps];
+                  const index = oldData.tableData.id;
+                  dataDelete.splice(index, 1);
+                  setComps([...dataDelete]);
+                  resolve();
+                }, 1000);
+              }),
+          }}
+        />
+      </ThemeProvider>
     </div>
   );
 };
