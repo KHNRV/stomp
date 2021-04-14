1413056a5fa1bbabae8f2baed3949d1fdd77e845
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-14 15:54:58 -0400

    remove unused icons

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,6 @@ import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
