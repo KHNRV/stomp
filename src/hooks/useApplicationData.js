2a4ba2fade2b1155ae0a9787f05a9a3482efc317
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-15 15:09:21 -0400

    Implement axios API actions

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -32,20 +32,78 @@ export default function useApplicationData() {
   }, []);
 
   const db = {
-    participant: {
-      create(participant) {},
-      update(participant) {},
-      delete(participant) {}
+    participants: {
+      create(participant) {
+        return axios
+          .post(`/api/events/${state.event_code}/participants`, participant)
+          .then((res) =>
+            setState((prev) => ({ ...prev, participants: res.data }))
+          );
+      },
+      update(participant) {
+        return axios
+          .put(
+            `/api/events/${state.event_code}/participants/${participant.id}`,
+            participant
+          )
+          .then((res) =>
+            setState((prev) => ({ ...prev, participants: res.data }))
+          );
+      },
+      delete(participant) {
+        return axios
+          .delete(
+            `/api/events/${state.event_code}/participants/${participant.id}`
+          )
+          .then((res) =>
+            setState((prev) => ({ ...prev, participants: res.data }))
+          );
+      },
     },
-    judge: {
-      create(judge) {},
-      update(judge) {},
-      delete(judge) {}
+    judges: {
+      create(judge) {
+        return axios
+          .post(`/api/events/${state.event_code}/judges`, judge)
+          .then((res) => setState((prev) => ({ ...prev, judges: res.data })));
+      },
+      update(judge) {
+        return axios
+          .put(`/api/events/${state.event_code}/judges/${judge.id}`, judge)
+          .then((res) => setState((prev) => ({ ...prev, judges: res.data })));
+      },
+      delete(judge) {
+        return axios
+          .delete(`/api/events/${state.event_code}/judges/${judge.id}`)
+          .then((res) => setState((prev) => ({ ...prev, judges: res.data })));
+      },
     },
     competitions: {
-      update(competitions) {},
-      create(competitions) {},
-      delete(competitions) {}
+      create(competition) {
+        return axios
+          .post(`/api/events/${state.event_code}/competitions`, competition)
+          .then((res) =>
+            setState((prev) => ({ ...prev, competitions: res.data }))
+          );
+      },
+      update(competition) {
+        return axios
+          .put(
+            `/api/events/${state.event_code}/competitions/${competition.id}`,
+            competition
+          )
+          .then((res) =>
+            setState((prev) => ({ ...prev, competitions: res.data }))
+          );
+      },
+      delete(competition) {
+        return axios
+          .delete(
+            `/api/events/${state.event_code}/competitions/${competition.id}`
+          )
+          .then((res) =>
+            setState((prev) => ({ ...prev, competitions: res.data }))
+          );
+      },
     },
   };
 
