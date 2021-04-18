d016e16cee695106295952243b211edf5d2bdaf5
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-18 01:48:10 -0400

    implement initial login logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
+
 import {
   Avatar,
   Button,
+  Box,
   CssBaseline,
   Grid,
   Link,
@@ -10,6 +12,7 @@ import {
   Typography,
 } from "@material-ui/core/";
 import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundPosition: "center",
   },
   paper: {
-    margin: theme.spacing(8, 4),
+    margin: theme.spacing(4, 4),
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -42,10 +45,40 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  // This is for Login Help
+  main: {
+    marginTop: "5em",
+    backgroundColor: "rgba(253, 200, 69, .3)",
+    border: `2px solid rgba(253, 200, 69, .5)`,
+    padding: "2em",
+    borderRadius: "1em",
+  },
 }));
 
 export default function Login() {
   const classes = useStyles();
+  const history = useHistory();
+  const [status, setStatus] = useState(null);
+
+  const loginEmail = useRef();
+  const loginPassword = useRef();
+
+  const login = useCallback(() => {
+    setStatus(null);
+    if (loginEmail.current.value !== "test@stomp.com") {
+      setTimeout(() => {
+        setStatus("invalidEmail");
+      }, 10);
+    } else if (loginPassword.current.value !== "123") {
+      setTimeout(() => {
+        setStatus("invalidPassword");
+      }, 10);
+    } else {
+      setTimeout(() => {
+        history.push("/");
+      }, 150);
+    }
+  }, [loginEmail, loginPassword, setStatus]);
 
   return (
     <Grid container component="main" className={classes.root}>
@@ -53,15 +86,26 @@ export default function Login() {
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
+        <Box  mr={2}>
+          <img height="100" src="/logo-dark.png" alt="" />
+        </Box>
           <Avatar className={classes.avatar}>
             <img height="30" src="/buttons/lock.svg" alt="login" />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <form className={classes.form} noValidate>
+          <form
+            className={classes.form}
+            onSubmit={(e) => {
+              e.preventDefault();
+              login();
+            }}
+          >
             <TextField
               variant="outlined"
+              inputRef={loginEmail}
+              error={status === "invalidEmail"}
               margin="normal"
               required
               fullWidth
@@ -70,9 +114,21 @@ export default function Login() {
               name="email"
               autoComplete="email"
               autoFocus
+              onChange={() => {
+                if (status === "invalidEmail") {
+                  setStatus(null);
+                }
+              }}
+              helperText={
+                status === "invalidEmail" &&
+                "This email address isn't associated with an account."
+              }
+              FormHelperTextProps={{ error: true }}
             />
             <TextField
               variant="outlined"
+              inputRef={loginPassword}
+              error={status === "invalidPassword"}
               margin="normal"
               required
               fullWidth
@@ -81,6 +137,19 @@ export default function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
+              onChange={() => {
+                if (status === "invalidPassword") {
+                  setStatus(null);
+                }
+              }}
+              helperText={
+                status === "invalidPassword" ? (
+                  <span>Incorrect password. Please try again, or Sign Up</span>
+                ) : (
+                  ""
+                )
+              }
+              FormHelperTextProps={{ error: true }}
             />
             <Button
               type="submit"
@@ -98,6 +167,14 @@ export default function Login() {
                 </Link>
               </Grid>
             </Grid>
+            {/* This is for Login Help */}
+            <div className={classes.main}>
+              <Typography variant="body2">
+                Email is: <b>test@stomp.com</b>
+                <br />
+                Password is: <b>123</b>
+              </Typography>
+            </div>
           </form>
         </div>
       </Grid>
