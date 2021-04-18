f8366a5894b112722679c754a14243c658c7b1df
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-04-18 16:45:16 -0400

    Remove mock data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Nav from "./components/Nav";
-import Dashboard from "./components/Dashboard";
 import DashboardCompetitions from "./components/DashboardCompetitions";
 import DashboardParticipants from "./components/DashboardParticipants";
 import DashboardJudges from "./components/DashboardJudges";
@@ -13,23 +12,13 @@ import TableScoring from "./components/TableScoring";
 import TableResults from "./components/TableResults";
 import TableStepper from "./components/TableStepper";
 
-import { impData } from "./helpers/testdata";
-import getData from "./helpers/getData";
-
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 import "./styles/app.scss";
 import useApplicationData from "./hooks/useApplicationData";
 
-// Feed in the the whole data and competition id
-let [compData, compCol] = getData(impData, 0);
-
 function App() {
   const action = useApplicationData();
-
-  const [comp, setComp] = useState(compData);
-  const columns = compCol;
-
   return (
     <>
       <div className="App">
@@ -76,11 +65,7 @@ function App() {
 
                   <TableStepper />
                   {!action.read.state.competitions.length ? null : (
-                    <TableResults
-                      data={comp}
-                      columns={columns}
-                      action={action}
-                    />
+                    <TableResults action={action} />
                   )}
                 </Route>
                 <Route exact path="/login">
